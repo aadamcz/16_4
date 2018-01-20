c9eabfb6515ec58d4427d7eaa19c3e2e7572a274
aadamcz <33779500+aadamcz@users.noreply.github.com>
2018-01-20 19:56:36 +0100

    Update Title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,8 +3,8 @@ import React from "react";
 const Title = ({todoNumber}) => {
 	return (
 		<div>
-		<h1>TodoList Name</h1>
-		<h2>You have ({todoNumber}) tasks</h2>	
+			<h1>TodoList Name</h1>
+			<h2>You have ({todoNumber}) tasks</h2>	
 		</div>
 	);
 }
